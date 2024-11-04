a2ce0d010233b2d9c64226386c23a4632ae60913
ajaykumar2pp <ajay2kumarpp@gmail.com>
2024-11-04 20:44:39 +0530

    reder google auth

--- src/passport/passport.js
+++ src/passport/passport.js
@@ -35,7 +35,7 @@ passport.use(new LocalStrategy({ usernameField: 'email' }, async (email, passwor
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/callback",
+    callbackURL: "https://gkv-ac-in.onrender.com/auth/google/callback",
 },
     async (accessToken, refreshToken, profile, done) => {
         // console.log(profile)
